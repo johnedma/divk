201e2deb73eae0f8b6071606cdca81747d9f8699
johned <johned.matt@gmail.com>
2016-10-09 19:11:44 -0400

    initial stylyings

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get("/", function(request, response){
+  response.sendFile("public/html/index.html", {root: __dirname});
+});
+
+server.listen(port, function(){
+  console.log("Now listening on port...", port);
+});
